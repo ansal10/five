6773473ec55b1848f7be0e7e8ca2e92d56dc4241
Anas MD <ansal@Anass-MacBook-Pro.local>
2017-04-05 14:08:14 +0530

    changing logger format

--- fiveapp/middleware/logger_middleware.py
+++ fiveapp/middleware/logger_middleware.py
@@ -1,12 +1,15 @@
 
 from django.utils.deprecation import MiddlewareMixin
 
+from fiveapp.utils import now
+
 
 class LoggerMiddleWare(MiddlewareMixin):
 
     def process_request(self, request):
+        print "\n\n(%s):\tRequest path : %s" % (now(), request.path)
         print "Request body : %s" % (request.body)
-        print "Request Meta : %s" % (request.META)
+        print "Request HTTP_AUTHORIZATION : %s" % (request.META.get('HTTP_AUTHORIZATION', None))
 
         return None
 
